e9bc5f8a97107f2dfdb350ea94d22d77408176c4
Arvinddurai <83328108+Arvinddurai@users.noreply.github.com>
2021-04-28 14:00:01 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,60 @@
+*{
+    margin:0;
+    padding:0;
+
+}
+.header
+{
+    height:100vh;
+}
+.navbar-style
+{
+    box-shadow: 0 5px 10px rgb(223, 7, 7);
+}
+.image
+{
+    height: 48px;
+    padding:2px 10px;
+}
+.icon-bar
+{
+    background: darkorange;
+}
+li a{
+    color: black;
+}
+.big-text
+{
+    font-size: 25px;
+    margin: 20px 0;
+    color :darkgoldenrod;
+}
+.btn
+{
+    margin: 30px;
+    width:150px;
+    padding: 10px;
+    border-radius: 20px;
+}
+.btn-1
+{
+    background-color: darkgoldenrod;
+    color: #fff;
+}
+a.btn-2
+{
+    background: transparent;
+    border: 1px solid darkgoldenrod;
+    color: #333;
+}
+a.btn:hover{
+    background: darkgoldenrod;
+    border: none;
+    color: #fff;
+    box-shadow: 5px  5px 10px ;
+    transition: 0.3s;
+}
+.a-info, .a-image
+{
+    margin: 30px;
+}
